3c9956ca5306838ef9e0d7f497202b5c688148fe
Sergio Henrique P. Oliveira <serginho@linx.com.br>
2023-04-12 13:55:05 -0300

    Teste unitário

---
+++ test/FluentValidationSample.UnitTests/Validators/PersonValidatorTests.cs
@@ -0,0 +1,68 @@
+﻿using FluentValidationSample.Models;
+using FluentValidation;
+using FluentValidation.TestHelper;
+
+namespace FluentValidationSample.Validators.Tests
+{
+    public class PersonValidatorTests : IClassFixture<PersonValidator>
+    {
+        private readonly IValidator<Person> _validator;
+
+        public PersonValidatorTests(PersonValidator validator)
+        {
+            _validator = validator;
+        }
+
+        [Fact()]
+        public void PersonValidator_Sem_Erros()
+        {
+            //Arrange
+            Person person = new() { Name = "Nome Teste", BirthDate = new DateTime(2000, 1, 1) };
+
+            //Act
+            var result = _validator.TestValidate(person);
+
+            //Assert
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+
+        [Fact]
+        public void PersonValidator_Deve_Retornar_Erro_Nome_Null()
+        {
+            //arrange
+            Person person = new() { Name = null, BirthDate = new DateTime(2000, 1, 1) };
+
+            //act
+            var result = _validator.TestValidate(person);
+
+            //assert
+            result.ShouldHaveValidationErrorFor(i => i.Name);
+        }
+
+        [Fact]
+        public void PersonValidator_Deve_Retornar_Erro_Nome_Vazio()
+        {
+            //arrange
+            Person person = new() { Name = "", BirthDate = new DateTime(2000, 1, 1) };
+
+            //act
+            var result = _validator.TestValidate(person);
+
+            //assert
+            result.ShouldHaveValidationErrorFor(i => i.Name);
+        }
+
+        [Fact]
+        public void PersonValidator_Deve_Retornar_Erro_Menor_18_Anos()
+        {
+            //arrange
+            Person person = new() { Name = "Nome Ok", BirthDate = DateTime.Now };
+
+            //act
+            var result = _validator.TestValidate(person);
+
+            //assert
+            result.ShouldHaveValidationErrorFor(i => i.BirthDate);
+        }
+    }
+}
